da745f237a3426bc43ed430f376c2fe36cd0c4ab
Awab Izzeldin <awabizzeldin02@gmail.com>
2025-11-03 16:11:16 +0300

    push

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -27,7 +27,7 @@ export default function ContactPage() {
     initial: { opacity: 0, y: 40 },
     animate: { opacity: 1, y: 0 },
     exit: { opacity: 0, y: -40 },
-    transition: { duration: 0.6, ease: "easeOut" },
+    transition: { duration: 0.6, ease: [0.42, 0, 0.58, 1] },
   };
 
   return (
