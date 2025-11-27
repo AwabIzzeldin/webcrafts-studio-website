8807a424e584443cf48f647dbb0df96f48ce6522
Awab Izzeldin <awabizzeldin02@gmail.com>
2025-11-27 17:47:07 +0800

    push

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,14 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const fadeUp = {
   hidden: { opacity: 0, y: 18 },
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.6, ease: "easeOut" },
+    transition: { duration: 0.6},
   },
 };
 
